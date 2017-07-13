21be8d6b27f4bdd26888512b9c05e96b5960a975
Yanagiezhawa <yanamulyana44@gmail.com>
2017-07-13 13:30:02 +0700

    Aplikasi TryOut

---
+++ Home_TryoutSDAsoka/Home/app/src/main/java/com/yanagiezhawa/home/Artikel.java
@@ -0,0 +1,14 @@
+package com.yanagiezhawa.home;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Artikel extends Fragment {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artikel);
+    }
+}
